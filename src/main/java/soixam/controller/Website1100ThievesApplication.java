c1c69724ca9fd3033075dce93c47648dfe88dbdd
unknown <duongducchinh160@gmail.com>
2022-12-13 13:58:47 +0700

    pro

---
+++ src/main/java/soixam/controller/Website1100ThievesApplication.java
@@ -0,0 +1,13 @@
+package soixam.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Website1100ThievesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Website1100ThievesApplication.class, args);
+    }
+
+}
